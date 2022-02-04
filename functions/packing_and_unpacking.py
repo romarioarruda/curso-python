93d1fa50336e5c7d6ca96cd1260fff1dd1622a6b
Romário Arruda <romarioarruda98@gmail.com>
2022-02-04 15:29:10 -0300

    funcao com paramentros variaveis

---
+++ functions/packing_and_unpacking.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+#  Packing é como se fosse o rest operator do JS
+def soma_n_valores(*numeros):
+    print(type(numeros))
+    total = 0
+    
+    for num in numeros:
+        total += num
+        
+    return total
+
+
+if __name__ == "__main__":
+    # Unpacking é como o spread operator do JS
+    tupla = (1,2,3,4,5,6,7,8,9)
+    print(soma_n_valores(*tupla))
