7d8d29bf6dd482d73d597b4db779ca73f6145cd1
Romário Arruda <romarioarruda98@gmail.com>
2022-02-14 09:39:04 -0300

    poo - herança

--- poo/data_v1.py
+++ poo/data_v1.py
@@ -4,14 +4,38 @@ class Data:
         self.mes = mes
         self.ano = ano
 
+    
+    def full_date(self):
+        full_month = self.return_full_months(self.mes)
+
+        return f'{self.dia} de {full_month} de {self.ano}'
+    
+    
+    def return_full_months(self, month_number):
+        months = {
+            1: 'Janeiro',
+            2: 'Fevereiro',
+            3: 'Março',
+            4: 'Abril',
+            5: 'Maio',
+        }
+
+        return months.get(month_number, "** Mês não reconhecido **")
+
 
     # Essa função força o retorno de string
     def __str__(self):
         return f'{self.dia}/{self.mes}/{self.ano}'
 
 
-data = Data(19, 7, 1995)
-data2 = Data(9, 1, 2022)
+
+class DataHeranca(Data):
+    def __init__(self, dia, mes, ano):
+        super().__init__(dia, mes, ano)
+
+
+data = DataHeranca(1, 2, 2022)
+data2 = DataHeranca(1, 1, 2022)
 
 print(data)
-print(data2)
+print(data2.full_date())
